8d43e7dd12b95dcea7853d496c91da1c1684b1e6
Christian Mayer <christian@fox21.at>
2021-04-18 13:58:43 +0200

    Example

--- example.rb
+++ example.rb
@@ -4,24 +4,23 @@ require 'uri'
 require 'net/http'
 require 'openssl'
 
-url = URI.parse('http://example.com')
-# url = URI.parse('https://example.com')
-# url = URI.parse('https://example.com/404')
+uri = URI.parse('http://example.com')
+# uri = URI.parse('https://example.com')
+# uri = URI.parse('https://example.com/404')
 
-puts "url: '#{url}'"
+puts "url: '#{uri}'"
 
-http = Net::HTTP.new(url.host, url.port)
+http = Net::HTTP.new(uri.host, uri.port)
 http.ssl_timeout = 5
-if url.scheme.to_s.downcase == 'https'
+if uri.scheme.to_s.downcase == 'https'
 	puts 'HTTPS request'
 	http.use_ssl = true
 	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 end
-req = Net::HTTP::Get.new(url)
-res = http.request(req)
+response = http.request(Net::HTTP::Get.new(uri))
 
-puts "response code: #{res.code}"
-if res.code.to_i == 200
+puts "response code: #{response.code}"
+if response.code.to_i == 200
 	puts 'response OK'
 else
 	puts 'response FAILED'
